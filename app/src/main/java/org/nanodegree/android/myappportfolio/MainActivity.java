cda3852311953b8dbab989e280171ba137247853
root <root@ip-10-47-174-141.ec2.internal>
2015-06-07 16:36:38 +0000

    Committed by Udacity

---
+++ app/src/main/java/org/nanodegree/android/myappportfolio/MainActivity.java
@@ -0,0 +1,66 @@
+package org.nanodegree.android.myappportfolio;
+
+import android.content.Context;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setOnClickListeners();
+    }
+
+    private void setOnClickListeners() {
+        findViewById(R.id.btnSpotifyStreamer).setOnClickListener(getClickListenerWithToast(R.string.sptf_streamer));
+        findViewById(R.id.btnScoresApp).setOnClickListener(getClickListenerWithToast(R.string.scores_app));
+        findViewById(R.id.btnLibraryApp).setOnClickListener(getClickListenerWithToast(R.string.library_app));
+        findViewById(R.id.btnBuildBigger).setOnClickListener(getClickListenerWithToast(R.string.build_bigger));
+        findViewById(R.id.btnXyzReader).setOnClickListener(getClickListenerWithToast(R.string.xyz_reader));
+        findViewById(R.id.btnCapstone).setOnClickListener(getClickListenerWithToast(R.string.capstone));
+    }
+
+    private View.OnClickListener getClickListenerWithToast(final int toastInfoId) {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = getApplicationContext();
+                CharSequence text = getString(R.string.toast_header) + getString(toastInfoId);
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        };
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
